de9b440bc2fe3a6a3b16fc700032ec9c79386387
cultstatue <saltedchurros@gmail.com>
2022-09-25 23:55:52 -0400

    added ability to create new cards

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,5 @@
-import "./form"
-import "./submit"
+import { toggleForm, clearForm } from "./form"
+import { fetchCards }from "./card"
 
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
@@ -13,7 +13,44 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { initDb, getDb, postDb } from './database';
 window.addEventListener('load', function () {
     initDb();
+    fetchCards()
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
+});
+
+  // Form functionality
+  const form = document.getElementById("formToggle");
+  const newContactButton = document.getElementById("new-contact");
+  let submitBtnToUpdate = false;
+  let profileId;
+  
+  newContactButton.addEventListener('click', event => {
+    toggleForm()
+   })
+  
+  form.addEventListener('submit', event => {
+    // Handle data
+    event.preventDefault();
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let profile = document.querySelector('input[type="radio"]:checked').value;
+  
+    // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+  if (submitBtnToUpdate == false) {
+    postDb(name, email, phone, profile);
+  } else {
+  
+    fetchCards();
+      // Toggles the submit button back to POST functionality
+    submitBtnToUpdate = false;
+  }
+  
+  // Clear form
+  clearForm();
+  // Toggle form
+  toggleForm();
+  // Reload the DOM
+  fetchCards();
 });
